fb83ac06887baee64d8afd6433e8f5109950706e
Jacob Hensley <jacobrhensley3@gmail.com>
2023-10-04 23:55:35 -0500

    edit more text yet again to test aws setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ReactTyped from "react-typed";
 function Title() {
   return (
     <div className='jumbotron text-center d-flex flex-column justify-content-center align-items-center'>
-      <div className='w-50 border rounded-4 p-3 text-center'>
+      <div className='w-md-50 border rounded-4 p-3 text-center m-4'>
         <p>est. 1995</p>
         <hr/>
         <h1 className='title-text display-4'>Jacob Hensley</h1>
@@ -19,27 +19,27 @@ function Title() {
 
 function About() {
   return (
-    <div className='container d-flex flex-row flex-wrap p-5'>
+    <div className='container d-flex flex-row justify-content-center flex-wrap p-5'>
       <div className='col-12 text-center mb-5'>
         <h2 className='display-4'>A little about me</h2>
       </div>
-        <div className='col-6 d-flex flex-row justify-content-center'>
-          <img className='rounded-5' width={250} height={250} src={mePhoto}/>
-        </div>
-        <div className='col-6'>
-          <h3>Who am I?</h3>
-          <p>
-            I am{" "}
-            <ReactTyped
-            strings={["a Dad", "a Husband", "a Software Engineer", "a Worship Drummer", "a Camping Enthusiast", "a Gameboy Nerd", "an AV Dweeb", "a curious mind"]}
-            typeSpeed={100}
-            loop
-            backSpeed={20}
-            cursorChar="|"
-            showCursor={true}
-            />
-          </p>
-        </div>
+      <div className='col-md-6 d-flex flex-row justify-content-center'>
+        <img className='rounded-5' width={250} height={250} src={mePhoto}/>
+      </div>
+      <div className='col-md-6 col-12 text-center text-md-start'>
+        <h3>Who am I?</h3>
+        <p>
+          I am{" "}
+          <ReactTyped
+          strings={["a Dad", "a Husband", "a Software Engineer", "a Worship Drummer", "a Camping Enthusiast", "a Gameboy Nerd", "an AV Dweeb", "a curious mind"]}
+          typeSpeed={100}
+          loop
+          backSpeed={20}
+          cursorChar="|"
+          showCursor={true}
+          />
+        </p>
+      </div>
     </div>
   )
 }
